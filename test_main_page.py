93bdf6479b7890c2ae224d255ba25db7781787e5
dizzy1978 <dizzy1978@gmail.com>
2021-06-04 02:33:48 +0300

    Initiation project

---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+# pytest -v --tb=line --language=en test_main_page.py # Выводим только одну строку из лога каждого упавшего теста.
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
