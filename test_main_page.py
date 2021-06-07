8dd31b5a216c49cf2000af87cf3314732fceb836
dizzy1978 <dizzy1978@gmail.com>
2021-06-07 14:16:07 +0300

    Page Object model example

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,19 @@
 # pytest -v --tb=line --language=en test_main_page.py # Выводим только одну строку из лога каждого упавшего теста.
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
+
+
+# Было:
+#def test_guest_can_go_to_login_page(browser):
+#    link = "http://selenium1py.pythonanywhere.com/"
+#    browser.get(link)
+#    login_link = browser.find_element_by_css_selector("#login_link")
+#    login_link.click()
