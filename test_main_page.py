1f9a68af55038eff480f241f5ce3ce369fcb29af
dizzy1978 <dizzy1978@gmail.com>
2021-06-07 15:11:01 +0300

    Page Object model example

--- test_main_page.py
+++ test_main_page.py
@@ -18,7 +18,8 @@ def test_guest_can_go_to_login_page(browser):
 #    login_link = browser.find_element_by_css_selector("#login_link")
 #    login_link.click()
 
-# Список методов PO, ассоциированных с одной страницей в отдельном классе
+# Список методов Page Object, ассоциированных с одной страницей в отдельном классе
+# В отдельном файле лежит класс page object, тест кейсы в файле с тест-кейсами.
 def test_add_to_cart(browser):
     page = ProductPage(url="", browser)   # инициализируем объект Page Object
     page.open()                           # открываем страницу в браузере
